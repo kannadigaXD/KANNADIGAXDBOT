8718d4b26b9aa3a2a73594eb9804d1dca8a50715
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-03-29 14:59:36 +0530

    KannadigaBot

--- FallenRobot/modules/debug.py
+++ FallenRobot/modules/debug.py
@@ -5,8 +5,8 @@ from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telethon import events
 
-from FallenRobot import dispatcher, telethn
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus
+from KannadigaBot import dispatcher, telethn
+from KannadigaBot.modules.helper_funcs.chat_status import dev_plus
 
 DEBUG_MODE = False
 

c0caf61b20bc29c74188ca52fb5d05d7a068bd81
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-03-29 16:05:54 +0530

    KannadigaBot

--- FallenRobot/utils/mongo.py
+++ FallenRobot/utils/mongo.py
@@ -2,7 +2,7 @@ from typing import Dict, Union
 
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
-from FallenRobot import MONGO_DB_URI
+from KannadigaBot import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
 db = mongo.FallenRobot

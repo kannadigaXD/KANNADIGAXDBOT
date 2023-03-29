11974970dfb60b38d38953949f9cf6b2f001090f
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-03-29 15:34:06 +0530

    KannadigaBot

--- FallenRobot/modules/telegraph.py
+++ FallenRobot/modules/telegraph.py
@@ -4,8 +4,8 @@ from datetime import datetime
 from PIL import Image
 from telegraph import Telegraph, exceptions, upload_file
 
-from FallenRobot import telethn as tbot
-from FallenRobot.events import register
+from KannadigaBot import telethn as tbot
+from KannadigaBot.events import register
 
 Anonymous = "Fallen"
 TMP_DOWNLOAD_DIRECTORY = "./"

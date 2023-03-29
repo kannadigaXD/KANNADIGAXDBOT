f86efa7984251ad39719068c23137e6e629d23d5
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-03-29 14:53:50 +0530

    KannadigaBot

--- FallenRobot/modules/cleaner.py
+++ FallenRobot/modules/cleaner.py
@@ -9,15 +9,15 @@ from telegram.ext import (
     run_async,
 )
 
-from FallenRobot import ALLOW_EXCL, CustomCommandHandler, dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.chat_status import (
+from KannadigaBot import ALLOW_EXCL, CustomCommandHandler, dispatcher
+from KannadigaBot.modules.disable import DisableAbleCommandHandler
+from KannadigaBot.modules.helper_funcs.chat_status import (
     bot_can_delete,
     connection_status,
     dev_plus,
     user_admin,
 )
-from FallenRobot.modules.sql import cleaner_sql as sql
+from KannadigaBot.modules.sql import cleaner_sql as sql
 
 CMD_STARTERS = ("/", "!") if ALLOW_EXCL else "/"
 BLUE_TEXT_CLEAN_GROUP = 13

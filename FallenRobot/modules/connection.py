31289f458d22acdd8c120ab1389cc8ca5dc9cccc
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-03-29 14:55:08 +0530

    KannadigaBot

--- FallenRobot/modules/connection.py
+++ FallenRobot/modules/connection.py
@@ -5,10 +5,10 @@ from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CallbackQueryHandler, CommandHandler, run_async
 
-import FallenRobot.modules.sql.connection_sql as sql
-from FallenRobot import DEV_USERS, DRAGONS, dispatcher
-from FallenRobot.modules.helper_funcs import chat_status
-from FallenRobot.modules.helper_funcs.alternate import send_message, typing_action
+import KannadigaBot.modules.sql.connection_sql as sql
+from KannadigaBot import DEV_USERS, DRAGONS, dispatcher
+from KannadigaBot.modules.helper_funcs import chat_status
+from KannadigaBot.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 

449a8fe9bad32783f5f254c5f59b9e85eb5c3704
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-03-29 14:47:21 +0530

    KannadigaBot

--- FallenRobot/modules/blacklist.py
+++ FallenRobot/modules/blacklist.py
@@ -6,18 +6,18 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters, MessageHandler, run_async
 from telegram.utils.helpers import mention_html
 
-import FallenRobot.modules.sql.blacklist_sql as sql
-from FallenRobot import LOGGER, dispatcher
-from FallenRobot.modules.connection import connected
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.alternate import send_message, typing_action
-from FallenRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from FallenRobot.modules.helper_funcs.extraction import extract_text
-from FallenRobot.modules.helper_funcs.misc import split_message
-from FallenRobot.modules.helper_funcs.string_handling import extract_time
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.sql.approve_sql import is_approved
-from FallenRobot.modules.warns import warn
+import KannadigaBot.modules.sql.blacklist_sql as sql
+from KannadigaBot import LOGGER, dispatcher
+from KannadigaBot.modules.connection import connected
+from KannadigaBot.modules.disable import DisableAbleCommandHandler
+from KannadigaBot.modules.helper_funcs.alternate import send_message, typing_action
+from KannadigaBot.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from KannadigaBot.modules.helper_funcs.extraction import extract_text
+from KannadigaBot.modules.helper_funcs.misc import split_message
+from KannadigaBot.modules.helper_funcs.string_handling import extract_time
+from KannadigaBot.modules.log_channel import loggable
+from KannadigaBot.modules.sql.approve_sql import is_approved
+from KannadigaBot.modules.warns import warn
 
 BLACKLIST_GROUP = 11
 

44e818a7d01c8120a24886ee4e6fa98a2cb59391
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-03-29 15:06:38 +0530

    KannadigaBot

--- FallenRobot/modules/fonts.py
+++ FallenRobot/modules/fonts.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from FallenRobot import pbot as Client
-from FallenRobot.utils.fonts import Fonts
+from KannadigaBot import pbot as Client
+from KannadigaBot.utils.fonts import Fonts
 
 
 @Client.on_message(filters.command(["font", "fonts"]))

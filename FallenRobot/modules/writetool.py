03730211aee434806a8947f46666b327347b9001
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-03-29 15:46:11 +0530

    KannadigaBot

--- FallenRobot/modules/writetool.py
+++ FallenRobot/modules/writetool.py
@@ -3,8 +3,8 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Upda
 from telegram.ext import CallbackContext
 from telegram.ext.dispatcher import run_async
 
-from FallenRobot import BOT_NAME, BOT_USERNAME, dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
+from KannadigaBot import BOT_NAME, BOT_USERNAME, dispatcher
+from KannadigaBot.modules.disable import DisableAbleCommandHandler
 
 
 @run_async

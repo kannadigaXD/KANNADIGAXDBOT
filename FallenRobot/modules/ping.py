fdc0c802e4c151b1424ca06d1a4e24578f25be05
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-03-29 15:25:49 +0530

    KannadigaBot

--- FallenRobot/modules/ping.py
+++ FallenRobot/modules/ping.py
@@ -3,8 +3,8 @@ import time
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
-from FallenRobot import StartTime, dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
+from KannadigaBot import StartTime, dispatcher
+from KannadigaBot.modules.disable import DisableAbleCommandHandler
 
 
 def get_readable_time(seconds: int) -> str:

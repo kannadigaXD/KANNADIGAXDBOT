72a937ab9adc4660db0e7106a0a46b0b6574d386
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-03-29 15:03:53 +0530

    KannadigaBot

--- FallenRobot/modules/disasters.py
+++ FallenRobot/modules/disasters.py
@@ -7,7 +7,7 @@ from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
 
-from FallenRobot import (
+from KannadigaBot import (
     DEMONS,
     DEV_USERS,
     DRAGONS,
@@ -17,15 +17,15 @@ from FallenRobot import (
     WOLVES,
     dispatcher,
 )
-from FallenRobot.modules.helper_funcs.chat_status import (
+from KannadigaBot.modules.helper_funcs.chat_status import (
     dev_plus,
     sudo_plus,
     whitelist_plus,
 )
-from FallenRobot.modules.helper_funcs.extraction import extract_user
-from FallenRobot.modules.log_channel import gloggable
+from KannadigaBot.modules.helper_funcs.extraction import extract_user
+from KannadigaBot.modules.log_channel import gloggable
 
-ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "FallenRobot/elevated_users.json")
+ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "KannadigaBot/elevated_users.json")
 
 
 def check_user_id(user_id: int, context: CallbackContext) -> Optional[str]:

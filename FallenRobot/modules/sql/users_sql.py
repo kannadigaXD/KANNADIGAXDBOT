fea0b2fbd2b92f4b1fa404a347696e49dc6c50e9
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-03-29 16:02:17 +0530

    KannadigaBot

--- FallenRobot/modules/sql/users_sql.py
+++ FallenRobot/modules/sql/users_sql.py
@@ -10,8 +10,8 @@ from sqlalchemy import (
     func,
 )
 
-from FallenRobot import dispatcher
-from FallenRobot.modules.sql import BASE, SESSION
+from KannadigaBot import dispatcher
+from KannadigaBot.modules.sql import BASE, SESSION
 
 
 class Users(BASE):

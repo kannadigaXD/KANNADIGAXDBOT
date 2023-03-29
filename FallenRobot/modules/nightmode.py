aed6ae9c93abb852f0c6107b1e37db7488fb4ce5
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-03-29 15:24:13 +0530

    KannadigaBot

--- FallenRobot/modules/nightmode.py
+++ FallenRobot/modules/nightmode.py
@@ -2,10 +2,10 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from telethon import functions, types
 from telethon.tl.types import ChatBannedRights
 
-from FallenRobot import BOT_NAME
-from FallenRobot import telethn as tbot
-from FallenRobot.events import register
-from FallenRobot.modules.sql.night_mode_sql import (
+from KannadigaBot import BOT_NAME
+from KannadigaBot import telethn as tbot
+from KannadigaBot.events import register
+from KannadigaBot.modules.sql.night_mode_sql import (
     add_nightmode,
     get_all_chat_id,
     is_nightmode_indb,

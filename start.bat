bcbc13c9a5cc35544c51f503f9579edafb153d04
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-04-01 15:36:59 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 TITLE Kannadiga Bot
-:: Enables virtual env mode and then starts Fallen
+:: Enables virtual env mode and then starts kannaidga
 env\scripts\activate.bat && py -m KannadigaBot

abac55ad5732b6e6175fad7e6e3e8960c144142c
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-04-01 15:56:47 +0530

    kannadiga

--- KannadigaBot/modules/telegraph.py
+++ KannadigaBot/modules/telegraph.py
@@ -7,7 +7,7 @@ from telegraph import Telegraph, exceptions, upload_file
 from KannadigaBot import telethn as tbot
 from KannadigaBot.events import register
 
-Anonymous = "Fallen"
+Agora = "Kannadiga"
 TMP_DOWNLOAD_DIRECTORY = "./"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=Anonymous)
